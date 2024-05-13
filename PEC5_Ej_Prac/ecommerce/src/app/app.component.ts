7430eb1b3869be024b03b82efc6bc1f4969576fe
jramma <pepe.ramos@mortensen.cat>
2024-05-13 19:32:35 +0200

    cursor-pointer

--- PEC5_Ej_Prac/ecommerce/src/app/app.component.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ArticleItemComponent } from './article-item/article-item.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ArticleItemComponent, ArticleListComponent],
+  imports: [NavbarComponent, CommonModule, RouterOutlet, ArticleItemComponent, ArticleListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
