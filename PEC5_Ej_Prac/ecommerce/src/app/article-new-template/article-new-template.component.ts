636c3c1c9a3a053434e72659d5920ee1ec723bb7
jramma <pepe.ramos@mortensen.cat>
2024-05-13 19:48:12 +0200

    ng g c components

---
+++ PEC5_Ej_Prac/ecommerce/src/app/article-new-template/article-new-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-new-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-new-template.component.html',
+  styleUrl: './article-new-template.component.css'
+})
+export class ArticleNewTemplateComponent {
+
+}
